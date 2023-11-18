c130dcb5a23bd90cefc306b2e3654e061ad43591
HecMurillo <xjuliopvp@gmail.com>
2023-11-17 22:52:22 -0700

    Se agrego el icono de la aplicacion, circular y rectangular

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.julitoelnoob.proyectofinal_myfit"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.julitoelnoob.proyectofinal_myfit"
         minSdk = 28
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
